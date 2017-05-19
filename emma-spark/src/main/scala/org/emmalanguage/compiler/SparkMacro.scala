ad5aaf01fa0ca57e59d6e9d45eace763ca197d72
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2017-05-19 02:35:25 +0200

    [API][IR] Added optional `cse` optimization to macros.
    In addition, renamed the currently CamelCased parameters
    in `reference.emma.conf` parameters an underscored style
    and moved optimization parameters to a dedicated scope.

--- emma-spark/src/main/scala/org/emmalanguage/compiler/SparkMacro.scala
+++ emma-spark/src/main/scala/org/emmalanguage/compiler/SparkMacro.scala
@@ -27,7 +27,7 @@ class SparkMacro(val c: blackbox.Context) extends MacroCompiler with SparkCompil
   def onSparkImpl1[T](e: c.Expr[T]): c.Expr[T] = {
     val cfg = loadConfig(configPaths())
     val res = pipeline(cfg)(e)
-    if (cfg.getBoolean("emma.compiler.printResult")) {
+    if (cfg.getBoolean("emma.compiler.print-result")) {
       c.warning(e.tree.pos, Core.prettyPrint(res))
     }
     c.Expr[T]((removeShadowedThis andThen unTypeCheck) (res))
@@ -36,7 +36,7 @@ class SparkMacro(val c: blackbox.Context) extends MacroCompiler with SparkCompil
   def onSparkImpl2[T](config: c.Expr[String])(e: c.Expr[T]): c.Expr[T] = {
     val cfg = loadConfig(configPaths(Some(config.tree)))
     val res = pipeline(cfg)(e)
-    if (cfg.getBoolean("emma.compiler.printResult")) {
+    if (cfg.getBoolean("emma.compiler.print-result")) {
       c.warning(e.tree.pos, Core.prettyPrint(res))
     }
     c.Expr[T]((removeShadowedThis andThen unTypeCheck) (res))
@@ -50,10 +50,13 @@ class SparkMacro(val c: blackbox.Context) extends MacroCompiler with SparkCompil
       Core.lift
     )
     // optional optimizing rewrites
-    if (cfg.getBoolean("emma.compiler.foldFusion")) {
+    if (cfg.getBoolean("emma.compiler.opt.cse")) {
+      xfms += Core.cse
+    }
+    if (cfg.getBoolean("emma.compiler.opt.fold-fusion")) {
       xfms += Optimizations.foldFusion
     }
-    if (cfg.getBoolean("emma.compiler.addCacheCalls")) {
+    if (cfg.getBoolean("emma.compiler.opt.auto-cache")) {
       xfms += Backend.addCacheCalls
     }
     // standard suffix
